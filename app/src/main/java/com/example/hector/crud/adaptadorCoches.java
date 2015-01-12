7cf5a64998a5574c48d989c2e5e81f8da2d842ee
HectorMEnciso <hectorencisolezcano@gmail.com>
2015-01-12 14:51:58 +0100

    CorrecionActualizacionAdaptador

--- app/src/main/java/com/example/hector/crud/adaptadorCoches.java
+++ app/src/main/java/com/example/hector/crud/adaptadorCoches.java
@@ -94,6 +94,7 @@ ahorrarnos el trabajo de inflar el layout definido cada vez que se muestra un nu
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
+        //carvar oor defeco
         holder.fot.setImageURI(co.get(position).getImageURI());
         holder.mat.setText(co.get(position).getMatricula());
         holder.mar.setText(co.get(position).getMarca());
